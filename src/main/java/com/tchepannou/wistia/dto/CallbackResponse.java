9d4ac179310ea8d0ce53585bf466c998c46a6dcf
htchepannou <htchepannou@expedia.com>
2015-08-03 19:57:50 -0400

    fix json

--- src/main/java/com/tchepannou/wistia/dto/CallbackResponse.java
+++ src/main/java/com/tchepannou/wistia/dto/CallbackResponse.java
@@ -3,11 +3,17 @@ package com.tchepannou.wistia.dto;
 public class CallbackResponse {
     private String status;
 
+    public CallbackResponse(){
+    }
     public CallbackResponse(String status){
         this.status = status;
     }
 
     public String getStatus() {
         return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
     }
 }
